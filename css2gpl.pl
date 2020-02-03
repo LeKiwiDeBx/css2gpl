3b54c4bf463f211e155b3a5ec93761fc1b33e694
LeKiwiDeBx <tatareau@free.fr>
2020-02-03 16:18:28 +0100

    reformat % liste couleurs nommées prototype doLineGpl + test

--- css2gpl.pl
+++ css2gpl.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w -s
-#  -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # program:    css2gpl.pl
 #             Extrait d'un fichier texte les couleurs (format css) et créé une
 #             palette .gpl pour The GIMP.
@@ -12,11 +12,13 @@
 #             devient la ligne gpl
 #               255 255 0 What...is your favorite color? blue, No, yel-- auuuuuugh!
 # version:    alpha 0.1
-# usage:      perl css2gpl.pl <nomFichier> <nomPalette.gpl>
+# usage:      perl css2gpl.pl -i=<nomFichier.css> -o=<nomFichierPalette[.gpl]> -n=<nomPalette> -c=<nbreColonne>
 # author:     LeKiwiDeBx
 # date:       27/01/2020
+# disclaimer: [=     This code is written by humans for humans ® (°}<Yeah!    =]
 #
-# Copyright  © 2020 </{LeKiwiDeBx}>  GNU General Public License
+#      Copyright  © 2020 </{LeKiwiDeBx}>  GNU General Public License
+#
 #
 #  This file is part of css2gpl.pl.
 #
@@ -36,177 +38,181 @@
 #  ((_/)o o(\_))
 #   `-'(. .)`-'
 #       \_/
+#                                                     GNU General Public License
 use strict;
 use warnings;
 use File::Basename;
 my $version = '0.1';
-my $fCss;
-my $fGpl;
+my $fCss;    # descripteur fichier CSS
+my $fGpl;    # descripteur fichier GPL
 my $Header =
   "GIMP Palette\nName: %s\nColumns: %s\n#\n";    # en tête du fichier GIMP
-## variable ligne de commande `my` interdit
+my %ColorComment = ();
+;    # ligne du fichier gpl :  <Rouge> <Vert> <Bleu> <Commentaires>
+
+# variable ligne de commande `my` interdit
 use vars qw/ $i /;    # fichier entrée texte ie -s="boCss.css"
 use vars qw/ $o /;    # fichier gpl sans extension .gpl ie -o="TheGpl"
 use vars qw/ $n /;    # nom de la palette ie: -n="namePalette"
 use vars qw/ $c /;    # nombre de columns a presenter -c=6
 my $patternComment  = ';\s*\/\*(.*)\*\/';    #extrait commentaire bout de ligne
 my %IDlistNameColor = (
-    'AliceBlue'            => 'F0F8FF',
-    'BlueViolet'           => '8A2BE2',
-    'AntiqueWhite'         => 'FAEBD7',
-    'Aqua'                 => '00FFFF',
-    'Aquamarine'           => '7FFFD4',
-    'Azure'                => 'F0FFFF',
-    'Beige'                => 'F5F5DC',
-    'Bisque'               => 'FFE4C4',
-    'Black'                => '000000',
-    'BlanchedAlmond'       => 'FFEBCD',
-    'Blue'                 => '0000FF',
-    'Brown'                => 'A52A2A',
-    'Chartreuse'           => '7FFF00',
-    'BurlyWood'            => 'DEB887',
-    'CadetBlue'            => '5F9EA0',
-    'Chocolate'            => 'D2691E',
-    'Coral'                => 'FF7F50',
-    'CornflowerBlue'       => '6495ED',
-    'Cornsilk'             => 'FFF8DC',
-    'Crimson'              => 'DC143C',
-    'Cyan'                 => '00FFFF',
-    'DarkBlue'             => '00008B',
-    'DarkCyan'             => '008B8B',
-    'DarkGoldenRod'        => 'B8860B',
-    'DarkGray'             => 'A9A9A9',
-    'DarkGrey'             => 'A9A9A9',
-    'DarkGreen'            => '006400',
-    'DarkKhaki'            => 'BDB76B',
-    'DarkMagenta'          => '8B008B',
-    'DarkOliveGreen'       => '556B2F',
-    'DarkOrange'           => 'FF8C00',
-    'DarkOrchid'           => '9932CC',
-    'DarkRed'              => '8B0000',
-    'DarkSalmon'           => 'E9967A',
-    'DarkSeaGreen'         => '8FBC8F',
-    'DarkSlateBlue'        => '483D8B',
-    'DarkSlateGray'        => '2F4F4F',
-    'DarkSlateGrey'        => '2F4F4F',
-    'DarkTurquoise'        => '00CED1',
-    'DarkViolet'           => '9400D3',
-    'DeepPink'             => 'FF1493',
-    'DeepSkyBlue'          => '00BFFF',
-    'DimGray'              => '696969',
-    'DimGrey'              => '696969',
-    'DodgerBlue'           => '1E90FF',
-    'FireBrick'            => 'B22222',
-    'FloralWhite'          => 'FFFAF0',
-    'ForestGreen'          => '228B22',
-    'Fuchsia'              => 'FF00FF',
-    'Gainsboro'            => 'DCDCDC',
-    'GhostWhite'           => 'F8F8FF',
-    'Gold'                 => 'FFD700',
-    'GoldenRod'            => 'DAA520',
-    'Gray'                 => '808080',
-    'Grey'                 => '808080',
-    'Green'                => '008000',
-    'GreenYellow'          => 'ADFF2F',
-    'HoneyDew'             => 'F0FFF0',
-    'HotPink'              => 'FF69B4',
-    'IndianRed'            => 'CD5C5C',
-    'Indigo'               => '4B0082',
-    'Ivory'                => 'FFFFF0',
-    'Khaki'                => 'F0E68C',
-    'Lavender'             => 'E6E6FA',
-    'LavenderBlush'        => 'FFF0F5',
-    'LawnGreen'            => '7CFC00',
-    'LemonChiffon'         => 'FFFACD',
-    'LightBlue'            => 'ADD8E6',
-    'LightCoral'           => 'F08080',
-    'LightCyan'            => 'E0FFFF',
-    'LightGoldenRodYellow' => 'FAFAD2',
-    'LightGray'            => 'D3D3D3',
-    'LightGrey'            => 'D3D3D3',
-    'LightGreen'           => '90EE90',
-    'LightPink'            => 'FFB6C1',
-    'LightSalmon'          => 'FFA07A',
-    'LightSeaGreen'        => '20B2AA',
-    'LightSkyBlue'         => '87CEFA',
-    'LightSlateGray'       => '778899',
-    'LightSlateGrey'       => '778899',
-    'LightSteelBlue'       => 'B0C4DE',
-    'LightYellow'          => 'FFFFE0',
-    'Lime'                 => '00FF00',
-    'LimeGreen'            => '32CD32',
-    'Linen'                => 'FAF0E6',
-    'Magenta'              => 'FF00FF',
-    'Maroon'               => '800000',
-    'MediumAquaMarine'     => '66CDAA',
-    'MediumBlue'           => '0000CD',
-    'MediumOrchid'         => 'BA55D3',
-    'MediumPurple'         => '9370DB',
-    'MediumSeaGreen'       => '3CB371',
-    'MediumSlateBlue'      => '7B68EE',
-    'MediumSpringGreen'    => '00FA9A',
-    'MediumTurquoise'      => '48D1CC',
-    'MediumVioletRed'      => 'C71585',
-    'MidnightBlue'         => '191970',
-    'MintCream'            => 'F5FFFA',
-    'MistyRose'            => 'FFE4E1',
-    'Moccasin'             => 'FFE4B5',
-    'NavajoWhite'          => 'FFDEAD',
-    'Navy'                 => '000080',
-    'OldLace'              => 'FDF5E6',
-    'Olive'                => '808000',
-    'OliveDrab'            => '6B8E23',
-    'Orange'               => 'FFA500',
-    'OrangeRed'            => 'FF4500',
-    'Orchid'               => 'DA70D6',
-    'PaleGoldenRod'        => 'EEE8AA',
-    'PaleGreen'            => '98FB98',
-    'PaleTurquoise'        => 'AFEEEE',
-    'PaleVioletRed'        => 'DB7093',
-    'PapayaWhip'           => 'FFEFD5',
-    'PeachPuff'            => 'FFDAB9',
-    'Peru'                 => 'CD853F',
-    'Pink'                 => 'FFC0CB',
-    'Plum'                 => 'DDA0DD',
-    'PowderBlue'           => 'B0E0E6',
-    'Purple'               => '800080',
-    'RebeccaPurple'        => '663399',
-    'Red'                  => 'FF0000',
-    'RosyBrown'            => 'BC8F8F',
-    'RoyalBlue'            => '041690',
-    'SaddleBrown'          => '8B4513',
-    'Salmon'               => 'FA8072',
-    'SandyBrown'           => 'F4A460',
-    'SeaGreen'             => '2E8B57',
-    'SeaShell'             => 'FFF5EE',
-    'Sienna'               => 'A0522D',
-    'Silver'               => 'C0C0C0',
-    'SkyBlue'              => '87CEEB',
-    'SlateBlue'            => '6A5ACD',
-    'SlateGray'            => '708090',
-    'SlateGrey'            => '708090',
-    'Snow'                 => 'FFFAFA',
-    'SpringGreen'          => '00FF7F',
-    'SteelBlue'            => '4682B4',
-    'Tan'                  => 'D2B48C',
-    'Teal'                 => '008080',
-    'Thistle'              => 'D8BFD8',
-    'Tomato'               => 'FF6347',
-    'Turquoise'            => '40E0D0',
-    'Violet'               => 'EE82EE',
-    'Wheat'                => 'F5DEB3',
-    'White'                => 'FFFFFF',
-    'WhiteSmoke'           => 'F5F5F5',
-    'Yellow'               => 'FFFF00',
-    'YellowGreen'          => '9ACD32'
+    "aliceblue"            => "F0F8FF",
+    "antiquewhite"         => "FAEBD7",
+    "aqua"                 => "00FFFF",
+    "aquamarine"           => "7FFFD4",
+    "azure"                => "F0FFFF",
+    "beige"                => "F5F5DC",
+    "bisque"               => "FFE4C4",
+    "black"                => "000000",
+    "blanchedalmond"       => "FFEBCD",
+    "blue"                 => "0000FF",
+    "blueviolet"           => "8A2BE2",
+    "brown"                => "A52A2A",
+    "burlywood"            => "DEB887",
+    "cadetblue"            => "5F9EA0",
+    "chartreuse"           => "7FFF00",
+    "chocolate"            => "D2691E",
+    "coral"                => "FF7F50",
+    "cornflowerblue"       => "6495ED",
+    "cornsilk"             => "FFF8DC",
+    "crimson"              => "DC143C",
+    "cyan"                 => "00FFFF",
+    "darkblue"             => "00008B",
+    "darkcyan"             => "008B8B",
+    "darkgoldenrod"        => "B8860B",
+    "darkgray"             => "A9A9A9",
+    "darkgrey"             => "A9A9A9",
+    "darkgreen"            => "006400",
+    "darkkhaki"            => "BDB76B",
+    "darkmagenta"          => "8B008B",
+    "darkolivegreen"       => "556B2F",
+    "darkorange"           => "FF8C00",
+    "darkorchid"           => "9932CC",
+    "darkred"              => "8B0000",
+    "darksalmon"           => "E9967A",
+    "darkseagreen"         => "8FBC8F",
+    "darkslateblue"        => "483D8B",
+    "darkslategray"        => "2F4F4F",
+    "darkslategrey"        => "2F4F4F",
+    "darkturquoise"        => "00CED1",
+    "darkviolet"           => "9400D3",
+    "deeppink"             => "FF1493",
+    "deepskyblue"          => "00BFFF",
+    "dimgray"              => "696969",
+    "dimgrey"              => "696969",
+    "dodgerblue"           => "1E90FF",
+    "firebrick"            => "B22222",
+    "floralwhite"          => "FFFAF0",
+    "forestgreen"          => "228B22",
+    "fuchsia"              => "FF00FF",
+    "gainsboro"            => "DCDCDC",
+    "ghostwhite"           => "F8F8FF",
+    "gold"                 => "FFD700",
+    "goldenrod"            => "DAA520",
+    "gray"                 => "808080",
+    "grey"                 => "808080",
+    "green"                => "008000",
+    "greenyellow"          => "ADFF2F",
+    "honeydew"             => "F0FFF0",
+    "hotpink"              => "FF69B4",
+    "indianred"            => "CD5C5C",
+    "indigo"               => "4B0082",
+    "ivory"                => "FFFFF0",
+    "khaki"                => "F0E68C",
+    "lavender"             => "E6E6FA",
+    "lavenderblush"        => "FFF0F5",
+    "lawngreen"            => "7CFC00",
+    "lemonchiffon"         => "FFFACD",
+    "lightblue"            => "ADD8E6",
+    "lightcoral"           => "F08080",
+    "lightcyan"            => "E0FFFF",
+    "lightgoldenrodyellow" => "FAFAD2",
+    "lightgray"            => "D3D3D3",
+    "lightgrey"            => "D3D3D3",
+    "lightgreen"           => "90EE90",
+    "lightpink"            => "FFB6C1",
+    "lightsalmon"          => "FFA07A",
+    "lightseagreen"        => "20B2AA",
+    "lightskyblue"         => "87CEFA",
+    "lightslategray"       => "778899",
+    "lightslategrey"       => "778899",
+    "lightsteelblue"       => "B0C4DE",
+    "lightyellow"          => "FFFFE0",
+    "lime"                 => "00FF00",
+    "limegreen"            => "32CD32",
+    "linen"                => "FAF0E6",
+    "magenta"              => "FF00FF",
+    "maroon"               => "800000",
+    "mediumaquamarine"     => "66CDAA",
+    "mediumblue"           => "0000CD",
+    "mediumorchid"         => "BA55D3",
+    "mediumpurple"         => "9370DB",
+    "mediumseagreen"       => "3CB371",
+    "mediumslateblue"      => "7B68EE",
+    "mediumspringgreen"    => "00FA9A",
+    "mediumturquoise"      => "48D1CC",
+    "mediumvioletred"      => "C71585",
+    "midnightblue"         => "191970",
+    "mintcream"            => "F5FFFA",
+    "mistyrose"            => "FFE4E1",
+    "moccasin"             => "FFE4B5",
+    "navajowhite"          => "FFDEAD",
+    "navy"                 => "000080",
+    "oldlace"              => "FDF5E6",
+    "olive"                => "808000",
+    "olivedrab"            => "6B8E23",
+    "orange"               => "FFA500",
+    "orangered"            => "FF4500",
+    "orchid"               => "DA70D6",
+    "palegoldenrod"        => "EEE8AA",
+    "palegreen"            => "98FB98",
+    "paleturquoise"        => "AFEEEE",
+    "palevioletred"        => "DB7093",
+    "papayawhip"           => "FFEFD5",
+    "peachpuff"            => "FFDAB9",
+    "peru"                 => "CD853F",
+    "pink"                 => "FFC0CB",
+    "plum"                 => "DDA0DD",
+    "powderblue"           => "B0E0E6",
+    "purple"               => "800080",
+    "rebeccapurple"        => "663399",
+    "red"                  => "FF0000",
+    "rosybrown"            => "BC8F8F",
+    "royalblue"            => "041690",
+    "saddlebrown"          => "8B4513",
+    "salmon"               => "FA8072",
+    "sandybrown"           => "F4A460",
+    "seagreen"             => "2E8B57",
+    "seashell"             => "FFF5EE",
+    "sienna"               => "A0522D",
+    "silver"               => "C0C0C0",
+    "skyblue"              => "87CEEB",
+    "slateblue"            => "6A5ACD",
+    "slategray"            => "708090",
+    "slategrey"            => "708090",
+    "snow"                 => "FFFAFA",
+    "springgreen"          => "00FF7F",
+    "steelblue"            => "4682B4",
+    "tan"                  => "D2B48C",
+    "teal"                 => "008080",
+    "thistle"              => "D8BFD8",
+    "tomato"               => "FF6347",
+    "turquoise"            => "40E0D0",
+    "violet"               => "EE82EE",
+    "wheat"                => "F5DEB3",
+    "white"                => "FFFFFF",
+    "whitesmoke"           => "F5F5F5",
+    "yellow"               => "FFFF00",
+    "yellowgreen"          => "9ACD32"
 );
 print
   "\nProgramme de création d'une palette .gpl à partir d'un fichier css\n";
 print "version: $version\n";
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# modulo function
-# usage: m * remainder (x,m) <=> (x)modulo(m)
+# Simule un modulo
+# usage: m*remainder(x,m) <=> (x)modulo(m)
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 sub remainder {
     my ( $a, $b ) = @_;
@@ -218,7 +224,7 @@ sub remainder {
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# ouvre en lecture le fichier CSS à analyser
+# Ouvre en lecture le fichier CSS à analyser
 # param: nom du fichier
 # return:
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -231,7 +237,7 @@ sub loadFileCss {
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# ouvre en ecriture le fichier GPL a crèer et ecrit l'en tête GPL
+# Ouvre en ecriture le fichier GPL a crèer et ecrit l'en tête GPL
 # param: nom du fichier
 # return:
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -246,7 +252,7 @@ sub writeHeaderFileGpl {
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# ouvre en lecture le fichier CSS à analyser
+# Ouvre en lecture le fichier CSS à analyser
 # param: nom du fichier
 # return: à l'ecran (version alpha) les coulerus et commentaire format GPL
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -415,7 +421,7 @@ sub extractRgbHsl {
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# Routine de conversion valeur HSL to RGB
+# Conversion valeur HSL à RGB
 # param: ligne en cours ddu fichier CSS
 # return: le format GPL de la couleur
 # sourcing:
@@ -445,7 +451,7 @@ sub hsl2Rgb {
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# sous-Routine de conversion valeur HSL to RGB appel de hsl2Rgb
+# Sous-Routine de conversion valeur HSL à RGB appel de hsl2Rgb
 # param: traitemet pour obtenir HUE
 # return: valeur parametrée
 # sourcing:
@@ -468,7 +474,7 @@ sub hue2Rgb {
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# Routine de conversion valeurs hexadecimales to RGB
+# Conversion valeurs hexadecimales to RGB
 # param: code hexadeimal de la couleur
 # return: le format GPL de la couleur
 # sourcing:
@@ -496,14 +502,39 @@ sub hexa2rgb {
     return $sRgb;
 }
 
+#
+#
+#
+sub doLineGpl {
+    my ( $color, $comment ) = @_;
+    unless ( exists( $ColorComment{$color} ) ) {
+        $ColorComment{$color} = $comment;
+    }
+    else {
+        $ColorComment{$color} .= $comment;
+    }
+
+    #print "\nrow : ", join( "\n", %ColorComment );
+}
+
 #  ___________________________________________________________________________
 # |                                                                           |
-# |                         </{Version α}>     °}                             |
+# |                         </{Version α}>     (°}<Ouais!                     |
 # |___________________________________________________________________________|
 my $File    = $i;    # recupere 1er argument
-my $FileGpl = $o;    # recupere 2eme argument
+my $FileGpl = $o;    # recupere 2e argument
 loadFileCss($File);
 print "\nEcriture du fichier gpl\n\n";
 writeHeaderFileGpl($FileGpl);
 readFileCss($File);
+
+#test doLineGpl
+my $colorTest_1   = "255 0 0";
+my $colorTest_2   = "jhjhkh";
+my $commentTest_1 = "commentaire 1";
+my $commentTest_2 = "commentaire 2";
+my $commentTest_3 = "commentaire 3";
+doLineGpl( $colorTest_1, $commentTest_1 );
+doLineGpl( $colorTest_1, $commentTest_2 );
+doLineGpl( $colorTest_2, $commentTest_3 );
 print "\n";
